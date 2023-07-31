5ee58cc8090f83ff0c42165fe02519dd5df4e946
yeongseolee204 <lyoungseo204@gmail.com>
2023-07-31 16:50:05 +0900

    서버에서 login 데이터를 받기 with body-parser

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -6,7 +6,8 @@ const router = express.Router();
 //불러오기
 const ctrl = require("./home.ctrl");
 
-router.get("/", ctrl.hello);  //ctrl 안에 있는 hello를 불러온다
-router.get("/login", ctrl.login);
+router.get("/", ctrl.output.home);
+router.get("/login", ctrl.output.login);
+router.post("/login", ctrl.process.login); //처리한다는 의미로 프로세스
 
 module.exports = router;
