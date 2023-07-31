811eff9b4a6eac3d87f93d03a0624844a6597eed
yeongseolee204 <lyoungseo204@gmail.com>
2023-07-31 13:15:00 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -13,4 +13,12 @@ function login() {
         id : id.value,
         psword : psword.value, //object는 : 로
     };
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req) //json으로 감싸줌. why? 요 위에서 json 데이터 타입으로 데이터를 전달할거기 때문
+    })
 }
